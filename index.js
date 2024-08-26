06ebe6c87e1fffdc5be6f4049b15a1414169db9c
GiMaRaMa <rrm.ingenieria.industrial@gmail.com>
2024-08-25 22:30:19 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,203 @@
+<!DOCTYPE HTML>
+<!--
+	FreelanceMatch by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>FreelanceMatch by HTML5 UP</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+	</head>
+	<body class="is-preload">
+		<!-- Wrapper -->
+			<div id="wrapper">
+				<!-- Header -->
+					<header id="header">
+						<div class="inner">
+							<!-- Logo -->
+								<a href="index.html" class="logo">
+									<span class="symbol"><img src="images/logo.svg" alt="" /></span><span class="title">FREELANCEMATCH</span>
+								</a>
+							<!-- Nav -->
+								<nav>
+									<ul>
+										<li><a href="#menu">Menu</a></li>
+									</ul>
+								</nav>
+						</div>
+					</header>
+
+				<!-- Menu -->
+					<nav id="menu">
+						<h2>Menu</h2>
+						<ul>
+							<li><a href="QUIENES SOMOS.html">QUIENES SOMOS</a></li>
+							<li><a href=".html">MISIÓN</a></li>
+							<li><a href="generic.html">VALORES ESTRATEGICOS</a></li>
+							<li><a href="generic.html">EQUIPO DIRECTIVO</a></li>
+						</ul>
+					</nav>
+					<button id="login-button">
+						<img src="images/login-icon.png" alt="Iniciar Sesión" style="width: 20px; height: 20px; vertical-align: middle;">
+				    </button>
+
+				<!-- BUSQUEDA -->
+				<html lang="es">
+					<head>
+						<meta charset="UTF-8">
+						<meta name="viewport" content="width=device-width, initial-scale=1.0">
+						<title>Botón de Búsqueda</title>
+						<link rel="stylesheet" href="styles.css">
+					</head>
+					<body>
+						<div id="search-wrapper">
+							<div id="search-container">
+								<input type="text" id="search-input" placeholder="BUSCA TU FREELANCER">
+								<button id="search-button">
+									Buscar
+								</button>
+							</div>
+						</div>
+					</body>
+					</html>
+				<!-- Main -->
+					<div id="main">
+						<div class="inner">
+							<header>
+								<h1>FreelanceMatch es una plataforma digital para emparejar proyectos freelance con los profesionales más adecuados. </a></h1>
+								<p>La plataforma ofrece a los clientes un acceso eficiente y confiable a talento especializado, mientras que proporciona a los freelancers mayores oportunidades y visibilidad</p>
+							</header>
+							<section class="tiles">
+								<article class="style1">
+									<span class="image">
+										<img src="images/pic01.jpg" alt="" />
+									</span>
+									<a href="generic.html">
+										<h2>Proyectos Ejecutados</h2>
+										<div class="content">
+											<p>Catologo de Proyectos Ejecutados.</p>
+										</div>
+									</a>
+								</article>
+								<article class="style2">
+									<span class="image">
+										<img src="images/pic02.jpg" alt="" />
+									</span>
+									<a href="generic.html">
+										<h2>Nuestros Freelancer</h2>
+										<div class="content">
+											<p>Catalogo de Profesionales en diversas áreas.</p>
+										</div>
+									</a>
+								</article>
+								<article class="style3">
+									<span class="image">
+										<img src="images/pic03.jpg" alt="" />
+									</span>
+									<a href="generic.html">
+										<h2>Herramientas para tus Proyectos</h2>
+										<div class="content">
+											<p>No Conoces lo Necesario para tu proyecto? Acá encontrarás una guía detallada acorde a tu proyecto.</p>
+										</div>
+									</a>
+								</article>
+								<article class="style4">
+									<span class="image">
+										<img src="images/pic04.jpg" alt="" />
+									</span>
+									<a href="generic.html">
+										<h2>Nuestros Clientes</h2>
+										<div class="content">
+											<p>Revisa los clientes que han confiado en nosotros.</p>
+										</div>
+									</a>
+								</article>
+								<article class="style5">
+									<span class="image">
+										<img src="images/pic05.jpg" alt="" />
+									</span>
+									<a href="generic.html">
+										<h2>MATCH DESTACADOS</h2>
+										<div class="content">
+											<p> CLIENTES Y FREELANCER.</p>
+										</div>
+									</a>
+								</article>
+								<article class="style6">
+									<span class="image">
+										<img src="images/pic06.jpg" alt="" />
+									</span>
+									<a href="generic.html">
+										<h2>Buscar Proyectos</h2>
+										<div class="content">
+											<p>Revisa los proyectos disponibles para tu interes.</p>
+										</div>
+									</a>
+								</article>
+							</section>
+						</div>
+					</div>
+
+				<!-- Footer -->
+					<footer id="footer">
+						<div class="inner">
+							<section>
+								<h2>Contáctanos</h2>
+								<form method="post" action="#">
+									<div class="fields">
+										<div class="field half">
+											<input type="text" name="name" id="name" placeholder="Name" />
+										</div>
+										<div class="field half">
+											<input type="email" name="email" id="email" placeholder="Email" />
+										</div>
+										<div class="field">
+											<textarea name="message" id="message" placeholder="Message"></textarea>
+										</div>
+									</div>
+									<ul class="actions">
+										<li><input type="submit" value="Send" class="primary" /></li>
+									</ul>
+								</form>
+							</section>
+							<section>
+								<h2>Follow</h2>
+								<ul class="icons">
+									<li><a href="#" class="icon brands style2 fa-twitter"><span class="label">Twitter</span></a></li>
+									<li><a href="#" class="icon brands style2 fa-facebook-f"><span class="label">Facebook</span></a></li>
+									<li><a href="#" class="icon brands style2 fa-instagram"><span class="label">Instagram</span></a></li>
+									<li><a href="#" class="icon brands style2 fa-dribbble"><span class="label">Dribbble</span></a></li>
+									<li><a href="#" class="icon brands style2 fa-github"><span class="label">GitHub</span></a></li>
+									<li><a href="#" class="icon brands style2 fa-500px"><span class="label">500px</span></a></li>
+									<li><a href="#" class="icon solid style2 fa-phone"><span class="label">Phone</span></a></li>
+									<li><a href="#" class="icon solid style2 fa-envelope"><span class="label">Email</span></a></li>
+								</ul>
+							</section>
+							<ul class="copyright">
+								<li>&copy; Untitled. All rights reserved</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+							</ul>
+						</div>
+					</footer>
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
+<header>
+	<h1>FreelanceMatch</h1>
+  </header>
+  <section>
+	<p>Desarrollado por rAmAgI</p>
+  </section>
+  
